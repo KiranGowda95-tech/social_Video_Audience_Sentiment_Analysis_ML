814b0c129df21245014938a20011f2b12d4c2c5d
Kiran <kirankumardn74@gmail.com>
2026-03-12 07:28:17 +0530

    data ingestion pipeline added

--- src/data/data_ingestion.py
+++ src/data/data_ingestion.py
@@ -12,7 +12,7 @@ logger =logging.getLogger('data_ingestion')
 logger.setLevel(logging.DEBUG)
 
 console_handler=logging.StreamHandler()
-console_handler.selLevel(logging.DEBUG)
+console_handler.setLevel(logging.DEBUG)
 
 file_handler=logging.FileHandler('errors.log')
 file_handler.setLevel(logging.ERROR)
@@ -26,6 +26,122 @@ logger.addHandler(file_handler)
 
 def load_params(params_path:str)->dict:
     try:
-        pass
+        with open(params_path,'r') as file:
+            params =yaml.safe_load(file)
+        logger.debug("Parameters retrieved from %s",params_path)
+        return params
+
     except FileNotFoundError:
         logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s',e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected erorr: %s',e)
+        raise
+
+
+# params=load_params("params.yaml")
+# print(params)
+
+def load_data(data_url:str)->pd.DataFrame:
+    try:
+        data=pd.read_csv(data_url)
+        # data=pd.read_json(data_url)
+        logger.debug('Data loaded from %s',data_url)
+        return data
+    
+    except FileNotFoundError:
+        logger.error('File not found: %s',data_url)
+        raise
+    except pd.errors.EmptyDataError:
+        logger.error('No data: %s',data_url)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s',e)
+        raise
+
+
+
+def preprocess_data(df:pd.DataFrame)->pd.DataFrame:
+    """Preprocess the data by handling missing values, duplicates, and empty strings."""
+    try:
+        #Removing missing values
+        df.dropna(inplace=True)
+        # Removing duplicates
+        df.drop_duplicates(inplace=True)
+        # Removing rows with empty strings
+        df=df[df["clean_comment"].str.strip() !='']
+
+        logger.debug('Data preprocessing completed: Missing values,duplicates and empty string')
+        return df
+    
+    except KeyError as e:
+        logger.error('Missing column in the dataframe: %s',e)
+        raise
+    
+    except Exception as e:
+        raise
+
+
+def save_data(train_data:pd.DataFrame,test_data:pd.DataFrame,data_path:str)->None:
+    """Save the train and test datasets,creating the raw folder if it doesn't exist."""
+    try:
+        raw_data_path=os.path.join(data_path,"raw")
+
+        #Create the data/raw directory if it does not exist
+        os.makedirs(raw_data_path,exist_ok=True)
+
+        #Save the train and test data
+        train_data.to_csv(os.path.join(raw_data_path,"train.csv"),index=False)
+        test_data.to_csv(os.path.join(raw_data_path,"test.csv"),index=False)
+
+        logger.debug('Train and test data saved to %s',raw_data_path)
+        
+    except Exception as e:
+        logger.error('Unexpected error occured while saving the data: %s',e)
+        raise
+
+def main():
+    try:
+        #Load parameters from the params.yaml in the root directory
+        params=load_params(params_path=os.path.join(os.path.dirname(os.path.abspath(__file__)),'../../params.yaml'))
+        print(params)
+        test_size=params['data_ingestion']['test_size']
+
+        #Load data from the specified URL
+        df=load_data(data_url='https://raw.githubusercontent.com/Himanshu-1703/reddit-sentiment-analysis/refs/heads/main/data/reddit.csv')
+
+        # Preprocess the data
+        final_df=preprocess_data(df)
+
+        # Split the data into training and testing sets
+        train_data,test_data=train_test_split(final_df,test_size=test_size,random_state=42)
+
+        #Save the split datasets and create the raw folder if it doesn't exist
+        save_data(train_data,test_data,data_path=os.path.join(os.path.dirname(os.path.abspath(__file__)),'../../data'))
+
+
+    except Exception as e:
+        logger.error('Failed to complete the data ingestion process: %s',e)
+        raise
+
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+data_url='https://raw.githubusercontent.com/Himanshu-1703/reddit-sentiment-analysis/refs/heads/main/data/reddit.csv'
+
+data=load_data(data_url)
+
+print(data)
